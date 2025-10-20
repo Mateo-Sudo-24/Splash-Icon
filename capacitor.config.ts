62756bfc187fa829c473d216401ea0eb2d19adfe
Mateo-Sudo-24 <mateo.paredes@epn.edu.ec>
2025-10-19 23:46:45 -0500

    Commit Icono Splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Splash-Icon',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+     SplashScreen: {
+      launchShowDuration: 5000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "FIT_CENTER",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    }
+  }
 };
 
 export default config;
